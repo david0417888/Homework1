7ea866c3b56747661595d9eb36b4729f095f263a
guanyu <2514tdguh@gmail.com>
2024-03-12 14:51:26 +0800

    Louis dada

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    uint256 private ; // A state variable to keep track of the number of registered students
 
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        studentCount += 1; // Increment the student count to generate a new unique ID for each student
+        return studentCount; // Return the new student ID
     }
 }
 
@@ -18,6 +20,7 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        return 456;
     }
 }
 
@@ -25,5 +28,7 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        return 789;
     }
+    // Implementation for the isEnrolled function from the interface
 }
